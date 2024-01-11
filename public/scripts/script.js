95f6fd56cce6273084a88945356dca9efd644a35
Arshynbeek <135094895+Arshynbeek@users.noreply.github.com>
2024-01-11 18:59:53 +0600

    worked on dark mode

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -35,18 +35,42 @@ document.querySelector(".mode").addEventListener("click", () => {
     document.querySelector(".dark").classList.add("switch-mode");
     document.querySelector(".light").classList.remove("switch-mode");
 
-    document.documentElement.style.setProperty("--white", "#000000");
+    document.documentElement.style.setProperty("--white", "#2F2F2F");
     document.documentElement.style.setProperty("--black", "#FFFFFF");
     document.documentElement.style.setProperty("--grey", "#1E1E1E");
     document.documentElement.style.setProperty("--grey-1", "#ECECEC");
 
-    document.querySelector("#gif3").src = "public/images/dark1.gif"
-    document.querySelector("#gif1").src = "public/images/dark2.gif"
-    document.querySelector("#gif2").src = "public/images/dark4.gif"
-    document.querySelector("#gif4").src = "public/images/dark3.gif"
+    for (let i of document.getElementsByClassName("service-content")) {
+      i.classList.add("service-content-mode");
+    }
 
-    document.querySelectorAll(".technologies img").forEach(function(img) {
-      img.style.filter = "invert(100%)";
+    document.querySelector("#gif3").src = "public/images/dark1.gif";
+    document.querySelector("#gif1").src = "public/images/dark2.gif";
+    document.querySelector("#gif2").src = "public/images/dark4.gif";
+    document.querySelector("#gif4").src = "public/images/dark3.gif";
+
+    document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(100%)";
+    document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(100%)";
+    document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(100%)";
+
+    document.querySelector(".technologies:nth-child(4) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(100%)";
+    });
+    document.querySelector(".technologies:nth-child(8) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(100%)";
+    });
+    document.querySelector(".technologies:nth-child(12) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(100%)";
+    });
+
+    document.querySelector(".technologies:nth-child(4) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(0)";
+    });
+    document.querySelector(".technologies:nth-child(8) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(0)";
+    });
+    document.querySelector(".technologies:nth-child(12) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(0)";
     });
     
     mode = !mode;
@@ -58,17 +82,82 @@ document.querySelector(".mode").addEventListener("click", () => {
     document.documentElement.style.setProperty("--black", "#000000");
     document.documentElement.style.setProperty("--grey", "#ECECEC");
     document.documentElement.style.setProperty("--grey-1", "#1E1E1E");
+    
+    for (let i of document.getElementsByClassName("service-content")) {
+      i.classList.remove("service-content-mode");
+    }
 
-    document.querySelector("#gif3").src = "public/images/3.gif"
-    document.querySelector("#gif1").src = "public/images/5.gif"
-    document.querySelector("#gif2").src = "public/images/6.gif"
-    document.querySelector("#gif4").src = "public/images/4.gif"
+    document.querySelector("#gif3").src = "public/images/3.gif";
+    document.querySelector("#gif1").src = "public/images/5.gif";
+    document.querySelector("#gif2").src = "public/images/6.gif";
+    document.querySelector("#gif4").src = "public/images/4.gif";
+    
+    mode = !mode;
+  }
+});
 
-    document.querySelectorAll(".technologies img").forEach(function(img) {
-      img.style.filter = "invert(0%)";
+document.querySelector(".mode-menu").addEventListener("click", () => {
+  if (!mode) {
+    document.querySelector(".dark-menu").classList.add("switch-mode");
+    document.querySelector(".light-menu").classList.remove("switch-mode");
+
+    document.documentElement.style.setProperty("--white", "#2F2F2F");
+    document.documentElement.style.setProperty("--black", "#FFFFFF");
+    document.documentElement.style.setProperty("--grey", "#1E1E1E");
+    document.documentElement.style.setProperty("--grey-1", "#ECECEC");
+
+    for (let i of document.getElementsByClassName("service-content")) {
+      i.classList.add("service-content-mode");
+    }
+
+    document.querySelector("#gif3").src = "public/images/dark1.gif";
+    document.querySelector("#gif1").src = "public/images/dark2.gif";
+    document.querySelector("#gif2").src = "public/images/dark4.gif";
+    document.querySelector("#gif4").src = "public/images/dark3.gif";
+
+    document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(100%)";
+    document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(100%)";
+    document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(100%)";
+
+    document.querySelector(".technologies:nth-child(4) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(100%)";
+    });
+    document.querySelector(".technologies:nth-child(8) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(100%)";
+    });
+    document.querySelector(".technologies:nth-child(12) img").addEventListener("mouseout", () => {
+      document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(100%)";
+    });
+
+    document.querySelector(".technologies:nth-child(4) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(4) img").style.filter = "invert(0)";
+    });
+    document.querySelector(".technologies:nth-child(8) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(8) img").style.filter = "invert(0)";
+    });
+    document.querySelector(".technologies:nth-child(12) img").addEventListener("mouseover", () => {
+      document.querySelector(".technologies:nth-child(12) img").style.filter = "invert(0)";
     });
     
+    mode = !mode;
+  } else {
+    document.querySelector(".dark-menu").classList.remove("switch-mode");
+    document.querySelector(".light-menu").classList.add("switch-mode");
+    
+    document.documentElement.style.setProperty("--white", "#FFFFFF");
+    document.documentElement.style.setProperty("--black", "#000000");
+    document.documentElement.style.setProperty("--grey", "#ECECEC");
+    document.documentElement.style.setProperty("--grey-1", "#1E1E1E");
+    
+    for (let i of document.getElementsByClassName("service-content")) {
+      i.classList.remove("service-content-mode");
+    }
 
+    document.querySelector("#gif3").src = "public/images/3.gif";
+    document.querySelector("#gif1").src = "public/images/5.gif";
+    document.querySelector("#gif2").src = "public/images/6.gif";
+    document.querySelector("#gif4").src = "public/images/4.gif";
+    
     mode = !mode;
   }
 });
